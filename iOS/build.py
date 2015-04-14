7342e84b713abbaa8cf9e85081dc722697409549
Kosso <kosso1@gmail.com>
2015-04-14 16:27:24 +0100

    update

--- iOS/build.py
+++ iOS/build.py
@@ -97,7 +97,7 @@ def compile_js(manifest,config):
 
 	from tools import splice_code
 
-	assets_router = os.path.join(cwd,'Classes','ComMfoggSquarecameraModuleAssets.m')
+	assets_router = os.path.join(cwd,'Classes','TiSqModuleAssets.m')
 	splice_code(assets_router, 'asset', root_asset_content)
 	splice_code(assets_router, 'resolve_asset', module_asset_content)
 
